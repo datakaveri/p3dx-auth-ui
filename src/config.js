0d4f85ced28c52f4f5de55eaab2c2500d18fa4ce
Ubuntu <azureuser@AuthVM.zvhw2hqi4etutkvul2y00xke2c.rx.internal.cloudapp.net>
2026-06-15 10:32:51 +0000

    Updated config.js file

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
-export const APP_URL = import.meta.env.VITE_APP_URL || window.location.origin;
+export const APP_URL = import.meta.env.VITE_APP_URL || "https://login.p3dx.iudx.org.in";
